1ce4971ecbb753e061260a7632a46da117369d42
Espen Lever <eko15ele@student.lu.se>
2022-10-26 15:03:09 +0200

    new board, initial coordinate system

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-def main():
-    
+#48 tiles around the board
+#4 tiles for each colors' home
+
+pavement = {}
+for i in range(1,49):
+    pavement[i] = [f'pos {i}']
+
+red_goal = {}
+for i in range(1,5):
+    red_goal[i] = [f'goal {i}']
+
+green_goal = {}
+for i in range(1, 5):
+    green_goal[i] = [f'goal {i}']
+
+yellow_goal = {}
+for i in range(1, 5):
+    yellow_goal[i] = [f'goal {i}']
+
+blue_goal = {}
+for i in range(1, 5):
+    blue_goal[i] = [f'goal {i}']
+
+print(f'pavement {pavement}')
+print(f'red goal {red_goal}')
+print(f'green goal {green_goal}')
+print(f'yellow goal {yellow_goal}')
+print(f'blue goal {blue_goal}')
