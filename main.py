eeb9a47e8cef30493e4a862c865979f561ae02ae
Espen Lever <eko15ele@student.lu.se>
2022-10-28 13:12:32 +0200

    constructor algoritm and find function

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-#48 tiles around the board
-#4 tiles for each colors' goal
+# 48 tiles around the board
+# 4 tiles for each colors' goal
 
 from Square import Square
 
 # Square assigment algoritm
 pavement = []
-for row in range(1,14):
-    for col in range(1,14):
+for row in range(1, 14):
+    for col in range(1, 14):
         if row == 1:
-            if col <= 5 and col >=9:
-                s = Square (column = col, row = row)
-                pavement.append(s)
+            if col <= 5 and col >= 9:
+                pavement.append(Square(col, row))
         elif row >= 2 and row <= 4:
             if col == 5 or col == 9:
-                s = Square(column = col, row = row)
-                pavement.append(s)
+                pavement.append(Square(col, row))
         elif row == 5:
             if col <= 5 or col >= 9:
-                s = Square(column=col, row=row)
-                pavement.append(s)
+                pavement.append(Square(col, row))
         elif row >= 6 and row <= 8:
             if col == 1 or col == 13:
-                s = Square(column=col, row=row)
-                pavement.append(s)
+                pavement.append(Square(col, row))
         elif row == 9:
             if col <= 5 or col >= 9:
-                s = Square(column=col, row=row)
-                pavement.append(s)
+                pavement.append(Square(col, row))
         elif row >= 10 and row <= 12:
             if col == 5 or col == 9:
-                s = Square(column=col, row=row)
-                pavement.append(s)
+                pavement.append(Square(col, row))
         else:
             if col <= 5 and col >= 9:
-                s = Square(column=col, row=row)
-                pavement.append(s)
+                pavement.append(Square(col, row))
+
+    def find_square(col, row):
+        for s in pavement:
+            if col == s.column:
+                if row == s.row:
+                    return s
+        return None
+
 
 print(pavement)
--- square.py
+++ square.py
@@ -1,9 +1,12 @@
 class Square:
     entrance: Square
-    exits: Square
-    column = 0
-    row = 0
+    exits:[Square]
 
     def __init__(self, column, row):
         self.column = column
         self.row = row
+    
+    def parse_coordinate(self.column, self.row):
+        self.coordinates = f"grid-area: {self.column} / {self.row}"
+
+    
